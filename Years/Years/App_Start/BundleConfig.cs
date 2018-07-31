3be3501fbb410421b77ecb989de0d1ab988e78d4
pingfan121 <956184289@qq.com>
2018-07-31 21:07:42 +0800

    111

--- Years/Years/App_Start/BundleConfig.cs
+++ Years/Years/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace Years.WebUI
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+          
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
