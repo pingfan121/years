9c8aa236db5d8cdfec969c94141b8f741d19422e
pingfan121 <956184289@qq.com>
2018-09-01 20:33:00 +0800

    //11

--- Years/plane/gamejs/flygame.js
+++ Years/plane/gamejs/flygame.js
@@ -9,7 +9,7 @@ var main = {
 
     gameend: function ()
     {
-    }
+    },
 
 
 
@@ -20,22 +20,24 @@ var main = {
     {
         //两个物体上下左右的位置
         var left1 = obj1.x;
-var right1 = obj1.x + obj1.w;
-var top1 = obj1.y;
-var bottom1 = obj1.y + obj1.h;
+        var right1 = obj1.x + obj1.w;
+        var top1 = obj1.y;
+        var bottom1 = obj1.y + obj1.h;
+        var left2 = obj2.x;
+        var right2 = obj2.x + obj2.w;
+        var top2 = obj2.y;
+        var bottom2 = obj2.y + obj2.h;
 
-var left2 = obj2.x;
-var right2 = obj2.x + obj2.w;
-var top2 = obj2.y;
-var bottom2 = obj2.y + obj2.h;
-
-//判断是否发生碰撞
-if (right1 < left2 || bottom1 < top2 || left1 > right2 || top1 > bottom2) {
-    return false;
-} else {
-    return true;
-}
-            }
+        //判断是否发生碰撞
+        if (right1 < left2 || bottom1 < top2 || left1 > right2 || top1 > bottom2)
+        {
+             return false;
+        }
+        else
+        {
+                return true;
+        }
+    }
 
 }
 
